dc907a2b516daf359ae3cea8c53e3ae49895a720
McKinley <colbym@iastate.edu>
2019-09-03 19:49:38 -0500

    work up to 3b

--- Lab-02/mux2_structure.vhd
+++ Lab-02/mux2_structure.vhd
@@ -16,11 +16,10 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity mux2_structure is
-  generic(N : integer := 32);
-  port(i_A  : in std_logic_vector(N-1 downto 0);
-       i_B  : in std_logic_vector(N-1 downto 0);
+  port(i_A  : in std_logic;
+       i_B  : in std_logic;
        i_S  : in std_logic;
-       o_F  : out std_logic_vector(N-1 downto 0));
+       o_F  : out std_logic);
 
 end mux2_structure;
 
@@ -43,26 +42,26 @@ component org2
        o_F  : out std_logic);
 end component;
 
-signal inv_S, i_AS, i_BIS
+signal inv_S, i_AS, i_BIS: std_logic;
 
 begin
-inv_s: invg 
-    port map(i_A  => i_S,
-  	     o_F  => inv_S);
-mux: for i in 0 to N-1 generate
-   andBIS_i: andg2 
-    port map(i_A  => i_B(i),
-             i_B  => inv_S,
-  	          o_F  => i_BIS(i));
- andAS_i: andg2 
-    port map(i_A  => i_A(i),
-             i_B  => i_S,
-  	          o_F  => i_AS(i));
- or_i: andg2 
-    port map(i_A  => i_AS(i),
-             i_B  => i_BIS(i),
-  	          o_F  => o_F(i));
-end generate;
 
+--compute ~S
+invg_S: invg port map(i_A  => i_S, o_F  => inv_S);
+--compute B & ~S
+andBIS_i: andg2 
+    port map(i_A  => i_B,
+             i_B  => inv_S,
+  	          o_F  => i_BIS);
+--compute A & S
+ andAS_i: andg2 
+    port map(i_A  => i_A,
+             i_B  => i_S,
+  	          o_F  => i_AS);
+--compute (A & S) | (B & ~S)
+ or_i: andg2 
+    port map(i_A  => i_AS,
+             i_B  => i_BIS,
+  	          o_F  => o_F);
   
 end structure;
