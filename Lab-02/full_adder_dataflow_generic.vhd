416fd888fea22ce34b9661a75ea0c5aeaed0ea01
McKinley <colbym@iastate.edu>
2019-09-05 11:56:11 -0500

    adding dataflow adder

--- Lab-02/full_adder_dataflow_generic.vhd
+++ Lab-02/full_adder_dataflow_generic.vhd
@@ -7,7 +7,7 @@
 
 -- full_adder_dataflow_generic.vhd
 -------------------------------------------------------------------------
--- DESCRIPTION: This file implements a generic full adder using structure
+-- DESCRIPTION: This file implements a generic full adder using dataflow
 -- architecture
 
 -------------------------------------------------------------------------
@@ -26,6 +26,9 @@ generic(N : integer := 32);
 end full_adder_dataflow_generic;
 
 architecture dataflow of full_adder_dataflow_generic is
-begin
-  
-end dataflow;v
+	begin
+	  adder: for i in 0 to N-1 generate
+		o_S(i) <= i_C xor (i_A(i) xor i_B(i));
+		o_C <= (i_A(i) and i_B(i)) or (i_B(i) and i_C) or (i_A(i) and i_C);
+		end generate
+	end dataflow;
